7af12fd426d6d9cd6e077d92faf8d364ab68b2ee
pramod <gopinath.pramod@gmail.com>
2023-01-23 09:46:14 +0530

    index change

---
+++ nechu.js
@@ -0,0 +1,12 @@
+let x = new Date(); 
+let y = x.getFullYear();
+document.getElementById("year").innerHTML=y;
+
+
+function showForm() {
+    document.getElementById("mytokenform").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("mytokenform").style.display = "none";
+}
